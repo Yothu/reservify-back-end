e06cf7be05ea2faa7e14d09c66a3b4fe177a84bc
fherrerao <fherrera0206@gmail.com>
2022-06-02 17:58:49 -0500

    Fix linter errors

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.1.1'
 
-#Gems to create login with devise JWT
+# Gems to create login with devise JWT
 gem 'devise'
 gem 'devise-jwt'
 gem 'rack-cors'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
 end
