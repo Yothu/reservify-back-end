3d740aabe5a62043d01cc25314182cfe18e36ec8
fherrerao <fherrera0206@gmail.com>
2022-06-02 17:53:16 -0500

    Create model and asociations on hote, reservation and user

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,4 @@
 class Reservation < ApplicationRecord
+  belongs_to :user
+  belongs_to :hotel
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :reservations
+  has_many :hotels
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+end
