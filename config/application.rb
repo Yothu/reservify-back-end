f2ee9fd7870d3a16e25322e4bf80d54d7d2688ab
fherrerao <fherrera0206@gmail.com>
2022-06-03 11:43:39 -0500

    Edit the routes for devise users

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module ReservifyBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  get '/member-data', to: 'members#show'
+    
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
