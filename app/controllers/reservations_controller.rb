6bb25eac28f07fa20ec57fd03bf6a50c711f1781
fherrerao <fherrera0206@gmail.com>
2022-06-06 11:22:48 -0500

    Show reservations for the current_user and fix linter errors

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,70 +1,54 @@
 class ReservationsController < ApplicationController
-  before_action :set_hotel, only: [:show, :edit, :update, :destroy]
-    before_action :authenticate_user!
+  before_action :authenticate_user!
 
-    
-    # GET /reservations
-    # GET /reservations.json
-    def index
-        @reservations = Reservation.all
-        render json: @reservations
-    end
-    
-    # GET /reservations/1
-    # GET /reservations/1.json
-    def show
-        @reservation = Reservation.find(params[:id])
-        render json: @reservation
-    end
-    
-    # GET /reservations/new
-    def new
-        @reservation = Reservation.new
-    end
-    
-   
-   
-    # POST /reservations
-    # POST /reservations.json
-    def create
-        @reservation = Reservation.new(reservation_params)
-    
-        respond_to do |format|
-        if @reservation.save
-            format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
-            format.json { render :show, status: :created, location: @reservation }
-        else
-            format.html { render :new }
-            format.json { render json: @reservation.errors, status: :unprocessable_entity }
-        end
-        end
-    end
-    
-    # PATCH/PUT /reservations/1
-    # PATCH/PUT /reservations/1.json
-    def update
-        respond_to do |format|
-        if @reservation.update(reservation_params)
-            format.html { redirect_to @reservation, notice: 'Reservation was successfully updated.' }
-            format.json { render :show, status: :ok, location: @reservation }
-        else
-            format.html { render :edit }
-            format.json { render json: @reservation.errors, status: :unprocessable_entity }
-        end
-        end
-    end
-    
-    # DELETE /reservations/1
-    # DELETE /reservations/1.json
-    def destroy
-        @reservation.destroy
-        respond_to do |format|
-        format.html { redirect_to reservations }
-        end
-    end
+  def index
+    @reservations = current_user.reservations
+    render json: @reservations
+  end
 
-    def reservation_params
-        params.require(:reservation).permit(:start_date, :end_date, :hotel_id, :user_id)
+  def show
+    @reservation = Reservation.find(params[:id])
+    render json: @reservation
+  end
+
+  def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    respond_to do |format|
+      if @reservation.save
+        format.html { redirect_to @reservation, notice: 'Reservation was successfully created.' }
+        format.json { render :show, status: :created, location: @reservation }
+      else
+        format.html { render :new }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
     end
- 
+  end
+
+  def update
+    respond_to do |format|
+      if @reservation.update(reservation_params)
+        format.html { redirect_to @reservation, notice: 'Reservation was successfully updated.' }
+        format.json { render :show, status: :ok, location: @reservation }
+      else
+        format.html { render :edit }
+        format.json { render json: @reservation.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @reservation.destroy
+    respond_to do |format|
+      format.html { redirect_to reservations }
+    end
+  end
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date, :hotel_id, :user_id)
+  end
 end
--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -6,9 +6,8 @@ class Hotel < ApplicationRecord
   validates :address, presence: true
   validates :city, presence: true
   validates :country, presence: true
-  validates :room_price, presence: true, numericality: { only_decimal: true , greater_than: 0 }
-  validates :number_of_rooms, presence: true ,numericality: { only_integer: true, greater_than: 10 }
+  validates :room_price, presence: true, numericality: { only_decimal: true, greater_than: 0 }
+  validates :number_of_rooms, presence: true, numericality: { only_integer: true, greater_than: 10 }
   validates :stars, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
   validates :current_free_rooms, presence: true, numericality: { only_integer: true }
-
 end
