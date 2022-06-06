41959b72508eaefa048c44320c26a81f4bd914bc
fherrerao <fherrera0206@gmail.com>
2022-06-06 11:00:18 -0500

    Add name field to hotels and room number for reservations

--- db/migrate/20220602214256_create_hotels.rb
+++ db/migrate/20220602214256_create_hotels.rb
@@ -1,6 +1,7 @@
 class CreateHotels < ActiveRecord::Migration[7.0]
   def change
     create_table :hotels do |t|
+      t.string :name
       t.string :address
       t.string :city
       t.string :country
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_152733) do
   enable_extension "plpgsql"
 
   create_table "hotels", force: :cascade do |t|
+    t.string "name"
     t.string "address"
     t.string "city"
     t.string "country"
@@ -40,6 +41,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_152733) do
   create_table "reservations", force: :cascade do |t|
     t.date "check_in_date"
     t.date "check_out_date"
+    t.integer "room_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
