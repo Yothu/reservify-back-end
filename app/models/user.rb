38252748f4c54849cba015b3978f5253d565fa27
fherrerao <fherrera0206@gmail.com>
2022-06-06 10:59:25 -0500

    Add hotel, reservation, and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :reservations
-  has_many :hotels
+  has_many :reservations, dependent: :delete_all
+  has_many :hotels, dependent: :delete_all
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
