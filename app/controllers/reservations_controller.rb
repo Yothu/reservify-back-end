247bde879f6a3efe8482dcf0448ad90b8d1e4389
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-06 18:52:21 -0300

    create doc for endpoint /reservations index and show

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,12 +1,12 @@
 class ReservationsController < ApplicationController
   before_action :authenticate_user!
-  #   load_and_authorize_resource
-
+    load_and_authorize_resource
+    !api :GET, '/hotels/id/reservations' , 'List of all available reservations for a given user'
   def index
     @reservations = current_user.reservations
     render json: @reservations
   end
-
+  !api :GET, '/hotels/id/reservations/id' , 'Shows reservation for a given id'
   def show
     @reservation = Reservation.find(params[:id])
     render json: @reservation
@@ -15,7 +15,7 @@ class ReservationsController < ApplicationController
   def new
     @reservation = Reservation.new
   end
-
+  !api :POST, '/hotels/id/reservations', 'Add a new reservation to the database for a given user'
   def create
     @reservation = Reservation.new(reservation_params)
 
