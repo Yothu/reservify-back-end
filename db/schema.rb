a491fc3110da9273733cc6270f1993728fadcd4c
fherrerao <fherrera0206@gmail.com>
2022-06-06 15:48:26 -0500

    Add column role for user table

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_152733) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "role", default: "user"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
