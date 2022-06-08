f1fcc3cd8c89fbdec072b58fea4ca3fcec45bc7f
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-08 14:31:47 -0300

    jwt files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include JsonWebToken
+  before_action :authenticate_request
+  
+    private
+  
+    def authenticate_request
+      header = request.headers["Authorization"]
+      
+      header = header.split(" ").last if header
+      decoded = jwt_decode(header)
+      @current_user = User.find(decoded[:user_id])
+    end
 end
