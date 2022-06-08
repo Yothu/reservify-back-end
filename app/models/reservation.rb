88def0ba4a136ff61b39418f62b2f25a0c676732
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-08 19:05:36 -0300

    Fix spacing

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :hotel
 
- 
   validates :room_number, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
