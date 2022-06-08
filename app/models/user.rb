192ceba33127752a985caa1afe4bf030f08c493c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-08 14:28:27 -0300

    add user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    require "securerandom"
+
+    has_secure_password
+  
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :username, presence: true, uniqueness: true
 end
