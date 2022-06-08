bd31b2b484029dbdd61139415e1a72a4331247b9
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-08 18:38:27 -0300

    add endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   
   namespace :api, :defaults => {:format => :json} do
     namespace :v1 do
+      post '/hotelsbylocation', to: 'hotels#hotels_by_location'
+      get '/countries', to: 'hotels#countries'
+      post '/cities', to: 'hotels#cities_by_country'
       resources :hotels do
         resources :reservations
       end
