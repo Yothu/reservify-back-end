32cfb13dd6590c35750e7f5b37d3286b000157a7
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-08 19:05:57 -0300

    LInters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   require 'securerandom'
-has_many :reservations, dependent: :delete_all
-has_many :hotels, dependent: :delete_all
-
+  has_many :reservations, dependent: :delete_all
+  has_many :hotels, dependent: :delete_all
 
   has_secure_password
-
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
