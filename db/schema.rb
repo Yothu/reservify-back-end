1c141039f75205d82bcf1ec792fa60a0c502b60a
fherrerao <fherrera0206@gmail.com>
2022-06-08 19:04:35 -0500

    Add to the schema the image column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_08_212016) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_223112) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_08_212016) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "image_url"
     t.index ["user_id"], name: "index_hotels_on_user_id"
   end
 
