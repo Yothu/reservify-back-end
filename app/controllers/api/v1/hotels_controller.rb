46a0fc0bbd0d8235d7a8cd967ebd8e954eff4355
fherrerao <fherrera0206@gmail.com>
2022-06-09 15:06:30 -0500

    Fix get hotels in production

--- app/controllers/api/v1/hotels_controller.rb
+++ app/controllers/api/v1/hotels_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::HotelsController < ApplicationController
   def index
     @hotels = Hotel.all
-    render json: @hotels, methods: [:photo_url]
+    render json: @hotels
   end
 
   def show
