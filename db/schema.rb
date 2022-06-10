3410a3097bda7a550a818f3a99356e27887a6761
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-10 10:45:44 -0300

    add description column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_09_151041) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_10_134002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_09_151041) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "image_url"
+    t.text "description"
     t.index ["user_id"], name: "index_hotels_on_user_id"
   end
 
