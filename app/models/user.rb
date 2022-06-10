d33c74740c990212d2daa12da2a3b637eeabb8d1
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-10 19:13:37 -0300

    linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :password, presence: true
   validates :username, presence: true, uniqueness: true
 end
