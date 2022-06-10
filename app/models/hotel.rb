9805e3c132852b64bb859a5b8b3d7494256d7d1c
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-06-10 10:45:15 -0300

    add validation

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,6 +1,6 @@
 class Hotel < ApplicationRecord
   has_many :reservations, dependent: :delete_all
-  # belongs_to :user
+  belongs_to :user
   has_one_attached :image
 
   def photo_url
@@ -8,12 +8,13 @@ class Hotel < ApplicationRecord
   end
   # Ex:- :limit => 40
 
-  # validates :name, presence: true
-  # validates :address, presence: true
-  # validates :city, presence: true
-  # validates :country, presence: true
-  # validates :room_price, presence: true, numericality: { only_decimal: true, greater_than: 0 }
-  # validates :number_of_rooms, presence: true, numericality: { only_integer: true, greater_than: 10 }
-  # validates :stars, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
-  # validates :current_free_rooms, presence: true, numericality: { only_integer: true }
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+  validates :room_price, presence: true, numericality: { only_decimal: true, greater_than: 0 }
+  validates :number_of_rooms, presence: true, numericality: { only_integer: true, greater_than: 10 }
+  validates :stars, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
+  validates :current_free_rooms, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
 end
