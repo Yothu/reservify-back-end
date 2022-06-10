537c4192830a485ab3a726969b8eda96621dced9
fherrerao <fherrera0206@gmail.com>
2022-06-10 11:41:23 -0500

    Fix delete hotel and validate image and description

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -12,9 +12,10 @@ class Hotel < ApplicationRecord
   validates :address, presence: true
   validates :city, presence: true
   validates :country, presence: true
+  validates :image_url, presence: true
+  validates :description, presence: true
   validates :room_price, presence: true, numericality: { only_decimal: true, greater_than: 0 }
   validates :number_of_rooms, presence: true, numericality: { only_integer: true, greater_than: 10 }
   validates :stars, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
   validates :current_free_rooms, presence: true, numericality: { only_integer: true }
-  validates :description, presence: true
 end
