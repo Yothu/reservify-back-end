0f76b74b9672f93a1feb4209cc5d36b29d03a47c
Anibal Amoroso <daniel_amoroso31@hotmail.com>
2022-06-14 19:22:51 -0300

    remove room number validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,8 +4,6 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :hotel
 
-  validates :room_number, presence: true, numericality: { only_integer: true, greater_than: 0 }, uniqueness: true
-
   private
 
   def decrease_hotel_free_rooms
